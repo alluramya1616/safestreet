d75a556b7d2bca4432ddd3f955f844850785eccb
alluramya1616 <alluramyareddy99@gmail.com>
2025-03-06 16:21:10 +0530

    Home

--- website/src/Components/Home.jsx
+++ website/src/Components/Home.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Home.css";
+import "./Home.css"; // Import the CSS file
+
 const HomePage = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="home-container">
       {/* Navbar */}
-      <nav className="bg-blue-600 text-white p-4 flex justify-between">
-        <div className="text-xl font-bold">MyApp</div>
-        <div className="space-x-4">
-          <Link to="/dashboard" className="hover:underline">Dashboard</Link>
-          <Link to="/home" className="hover:underline">Home</Link>
-          <Link to="/logout" className="hover:underline">Log Out</Link>
-          <Link to="/about" className="hover:underline">About Us</Link>
-          <Link to="/help" className="hover:underline">Help</Link>
+      <nav className="navbar">
+        <div className="logo">MyApp</div>
+        <div className="nav-links">
+          <Link to="/home">Home</Link>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/help">Help</Link>
+          <Link to="/logout">Log Out</Link>
         </div>
       </nav>
 
       {/* Main Content */}
-      <main className="flex-grow p-8 text-center">
-        <h1 className="text-3xl font-bold mb-4">Welcome to MyApp</h1>
-        <p className="text-lg">This is the home page where you can find information about our application.</p>
+      <main className="main-content">
+        <h1>Welcome to MyApp</h1>
+        <p>This is the home page where you can find information about our application.</p>
       </main>
     </div>
   );
